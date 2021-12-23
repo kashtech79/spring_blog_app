5e19a64424abdbc0ea319bc6a9c00d7d643cfe99
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-12-23 21:22:59 +0100

    first commit

---
+++ src/main/java/com/kash/blog/entity/Post.java
@@ -0,0 +1,23 @@
+package com.kash.blog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "content", nullable = false)
+    private String content;
+}
