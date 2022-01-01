de6d2e86493a4d6c189aa379f0c3b0023d6ba582
kashifnizami@gmail.com <kashifnizami@gmail.com>
2022-01-01 18:38:19 +0100

    Until 49 lecture - update Comment by id

--- src/main/java/com/kash/blog/serviceImpl/CommentServiceImpl.java
+++ src/main/java/com/kash/blog/serviceImpl/CommentServiceImpl.java
@@ -71,6 +71,28 @@ public class CommentServiceImpl implements CommentService {
 
     }
 
+    @Override
+    public CommentDto updateComment(Long postId, long commentId, CommentDto commentRequest) {
+        // retrieve post entity by id
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundException("Post", "id", postId));
+
+        // retrieve comment by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() ->
+                new ResourceNotFoundException("Comment", "id", commentId));
+
+        if(!comment.getPost().getId().equals(post.getId())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belongs to post");
+        }
+
+        comment.setName(commentRequest.getName());
+        comment.setEmail(commentRequest.getEmail());
+        comment.setBody(commentRequest.getBody());
+
+        Comment updatedComment = commentRepository.save(comment);
+        return mapToDto(updatedComment);
+    }
+
     private CommentDto mapToDto(Comment comment){
         CommentDto commentDto = new CommentDto();
         commentDto.setId(comment.getId());
