e4a31c481db816930151aadc320c9af6ee2ee381
kashifnizami@gmail.com <kashifnizami@gmail.com>
2022-01-02 12:09:27 +0100

    Until 58 lecture - Global exception handling

--- src/main/java/com/kash/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kash/blog/exception/GlobalExceptionHandler.java
@@ -31,5 +31,13 @@ public class GlobalExceptionHandler {
     }
     // global exceptions
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                                        WebRequest webRequest){
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
