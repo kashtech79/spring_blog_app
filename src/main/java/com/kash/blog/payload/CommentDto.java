624bb54affd24713020757422e4b8faa5c9fa1c7
kashifnizami@gmail.com <kashifnizami@gmail.com>
2022-01-03 10:48:45 +0100

    Until 63 lecture - Implement validation in create & update

--- src/main/java/com/kash/blog/payload/CommentDto.java
+++ src/main/java/com/kash/blog/payload/CommentDto.java
@@ -2,10 +2,19 @@ package com.kash.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private long id;
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+    @NotEmpty(message = "Email should not be null")
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min = 10, message = "")
     private String body;
 }
